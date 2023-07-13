885a9cd1d103d144e0f33c0b8eee098778cd410e
ashtikasingh <aashtika2002@gmail.com>
2023-07-13 19:18:28 +0530

    chapter1

---
+++ 001_Practice.js
@@ -0,0 +1,37 @@
+//13 july 2023
+
+//Question 01
+let a ="STRING";
+let b = 6;
+console.log(a+b);
+
+//Question 02
+console.log(typeof(a+b));
+
+//Question 03
+const c ={
+    name: "Ash",
+    year: 4,
+    isGraduate: false
+}
+console.log(c);
+
+//c=1 -> no
+
+//Question 04
+c['fri'] = "ASHTIKA"
+c['name'] = "Singh"
+console.log(c);
+
+//Question 05
+const dict = {
+    special: "distinguished, set apart from, or excelling others of its kind",
+    ahu: "a stone heap or platform used by the Polynesians as a marker or memorial.",
+    indecorous: "violating generally accepted standards of good taste or propriety; unseemly.",
+    emoticon: "a digital icon or a sequence of keyboard symbols that serves to represent a facial expression, as :‐) for a smiling face.",
+    Stygian: "dark or gloomy."
+}
+
+console.log(dict);
+console.log(dict.ahu);
+console.log(dict["emoticon"]);
