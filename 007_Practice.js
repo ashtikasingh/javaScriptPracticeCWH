ca8666b98750a431431f60a4dcf4e2a4812c1e8e
ashtikasingh <aashtika2002@gmail.com>
2023-07-16 20:21:14 +0530

    Chapter 7

---
+++ 007_Practice.js
@@ -0,0 +1,16 @@
+//16 July 2023
+
+//QUESTION 1
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "red";
+
+// QUESTION 3
+document.getElementsByTagName("nav")[0].firstElementChild.style.color = "green";
+document.getElementsByTagName("nav")[0].lastElementChild.style.color = "green";
+
+//QUESTION 4
+Array.from(document.getElementsByTagName("li")).forEach((element)=>{
+    element.style.background = "yellow";
+})
+
+//QUESTION 5 -> d
+
